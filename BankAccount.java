e0e6f19ec2f7e975c7ab2e87bbaba1d65b659151
Fatima Zaidi <fazzusa@gmail.com>
2026-09-08 21:06:44 -0400

    added code into bank account and edited withdraw to check for min balance

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,33 @@
 package lab2;
 
 public class BankAccount {
+	private String accountHolder;
+	private double balance;
 
+	public BankAccount(String accountHolder, double balance) {
+		this.accountHolder = accountHolder;
+		this.balance = balance;
+	}
+
+
+	public void deposit(double amount) {
+		// TODO: add the amount to the balance
+	}
+
+	public boolean withdraw(double amount) {
+		// TODO:
+		// Withdraw the requested amount if sufficient funds are available.
+		// Return true if the withdrawal succeeds.
+		// Return false otherwise. return false;
+
+		if(balance >= 50) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	public String getAccountSummary() {
+		return accountHolder + ": $" + balance;
+	}
 }
